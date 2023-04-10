801ef79cc016bac61b9d5cee132360d210f16a72
fulano <fulano@zutano.com>
2023-04-10 18:27:11 +0200

    TRY app init

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { WagmiConfig } from 'wagmi';
@@ -13,7 +12,7 @@ import wagmiClient, { chains } from './Config/wagmiClient';
 import ContextProvider from './contexts';
 import { SWRConfig } from 'swr';
 import { Provider as JotaiProvider } from 'jotai';
-import { RainbowKitProvider,darkTheme } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 
 const options = {
   fetcher: (url: string) =>
@@ -23,17 +22,17 @@ const options = {
 import { inject } from '@vercel/analytics';
 inject();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
-        <HashRouter>
-          <SWRConfig value={options}>
-            <ContextProvider>
-              <JotaiProvider>
-                <App />
-              </JotaiProvider>
-            </ContextProvider>
-          </SWRConfig>
-        </HashRouter>
-      </RainbowKitProvider>
-    </WagmiConfig>
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains} theme={darkTheme()}>
+      <HashRouter>
+        <SWRConfig value={options}>
+          <ContextProvider>
+            <JotaiProvider>
+              <App />
+            </JotaiProvider>
+          </ContextProvider>
+        </SWRConfig>
+      </HashRouter>
+    </RainbowKitProvider>
+  </WagmiConfig>
 );
