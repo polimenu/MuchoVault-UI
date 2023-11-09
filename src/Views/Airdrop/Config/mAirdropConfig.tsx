a7be1c8750b9d77432cd8053edf21f10281064f4
fulano <fulano@zutano.com>
2023-11-09 17:33:48 +0100

    change mAirdrop address

--- src/Views/Airdrop/Config/mAirdropConfig.tsx
+++ src/Views/Airdrop/Config/mAirdropConfig.tsx
@@ -1,7 +1,7 @@
 export const MAIDROP_CONFIG = {
   42161: {
     ManagerContract: "0xd68391b25EaDA08b3b3DF5A7f94806f7dbbed819",
-    TokenContract: "0xF13a92cd15FC4ed9DE60C5BF5468606dFA688b89",
+    TokenContract: "0x96b8627b98232A767A0aC3f3bC5A0567f3156F16",
     TokenPayment: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
     TokenPaymentSymbol: "USDC",
     TokenPaymentDecimals: 6,
