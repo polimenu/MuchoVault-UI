2115d927eaf74029098fa01a6ae070f1d2629d33
polimenu <70945263+polimenu@users.noreply.github.com>
2024-04-23 13:51:50 +0200

    scroll

--- src/Views/Ramp/Components/CountriesDropDown.tsx
+++ src/Views/Ramp/Components/CountriesDropDown.tsx
@@ -21,7 +21,7 @@ export const CountriesDropDown = ({ setCountry, country }: { setCountry: any; co
     return (
         <BufferDropdown
             rootClass="w-fit inline mt-5 mb-5 !y-auto"
-            className="py-4 px-4 bg-2 h-[10vw] !y-auto"
+            className="py-4 px-4 bg-2 h-[300px] !y-auto"
             dropdownBox={(a, open, disabled) => (
                 <div
                     className={`flex items-center justify-between ${classes.fontSize} font-medium bg-[#2c2c41] pl-3 pr-[0] ${classes.verticalPadding} rounded-sm text-1`}
